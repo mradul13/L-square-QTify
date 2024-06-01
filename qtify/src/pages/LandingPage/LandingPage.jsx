d3be5434a1af306ef867a229f93c4e85e25d6ee6
mradul13 <mradulchourasiya@gmail.com>
2024-06-01 20:23:06 +0530

    songs section completed

--- qtify/src/pages/LandingPage/LandingPage.jsx
+++ qtify/src/pages/LandingPage/LandingPage.jsx
@@ -2,16 +2,18 @@ import {React, useEffect, useState} from "react";
 import {
 	fetchTopAlbums,
 	fetchNewAlbums,
-	// fetchAllSongs,
+	fetchAllSongs,
 } from "../../api/api.js";
 import { Toaster } from "react-hot-toast";
 import { errorHandler } from "../../config/helper-methods";
-// import { accordionData } from "../../config/helper-config";
+import { accordionData } from "../../config/helper-config";
 import styles from "./LandingPage.module.css";
 import Navbar from "../../components/Navbar/Navbar.jsx";
 import SearchBox from "../../components/SearchBox/SearchBox.jsx";
 import HeroSection from "../../components/HeroSection/HeroSection.jsx";
 import Section from "../../components/Section/Section.jsx";
+import FilterTabs from "../../components/FilterTabs/FilterTabs.jsx";
+import CustomAccordion from "../../components/Accordion/CustomAccordion.jsx";
 
 
 
@@ -19,7 +21,7 @@ function LandingPage() {
 
 	const [topAlbumData, setTopAlbumData] = useState([]);
 	const [newAlbumData, setNewAlbumData] = useState([]);
-	// const [allSongsData, setAllSongsData] = useState([]);
+	const [allSongsData, setAllSongsData] = useState([]);
 	const [loadingState, setLoadingState] = useState({
 		topAlbum: true,
 		newAlbum: true,
@@ -55,11 +57,24 @@ function LandingPage() {
 			errorHandler(error);
 		}
 	};
+	const generateAllSongsData = async () => {
+		try {
+			_manageLoadingState("allSongs", true);
+
+			const data = await fetchAllSongs();
+			setAllSongsData(data);
+
+			_manageLoadingState("allSongs", false);
+		} catch (error) {
+			_manageLoadingState("allSongs", false);
+			errorHandler(error);
+		}
+	};
 
 	useEffect(() => {
 		generateTopAlbumData();
 		generateNewAlbumData();
-		// generateAllSongsData();
+		generateAllSongsData();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
@@ -90,6 +105,26 @@ function LandingPage() {
 					type="album"
 					loadingState={loadingState.newAlbum}
 				/>
+			</div>
+			<hr className={styles.line}></hr>
+
+			<div className={styles.filter_songs_wrapper}>
+				<div>
+					<h3 className={styles.tabsTitle}>Songs</h3>
+				</div>
+				<FilterTabs data={allSongsData} loadingState={loadingState.allSongs} />
+			</div>
+			<hr className={styles.line}></hr>
+			<div className={styles.customAccordionWrapper}>
+				<h1 className={styles.accordionHeader}>FAQs</h1>
+
+				{accordionData?.length ? (
+					accordionData.map((each, index) => {
+						return <CustomAccordion key={index} data={each} />;
+					})
+				) : (
+					<></>
+				)}
 			</div>
 		</div>
 	);
